86b3d8112c67e832cb78a4b8bb76370999282fd7
Yuri Achermann <yuri.achermann@gmail.com>
2024-06-13 14:49:10 -0300

    Trabalho 2

---
+++ trabalho2/dynamicCode/_49193cb8b23425a89b3f775bf00963f4f973b89b/codeStreamTemplate.C
@@ -0,0 +1,109 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) YEAR OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Description
+    Template for use with codeStream.
+
+\*---------------------------------------------------------------------------*/
+
+#include "dictionary.H"
+#include "fieldTypes.H"
+#include "Ostream.H"
+#include "Pstream.H"
+#include "read.H"
+#include "unitConversion.H"
+
+//{{{ begin codeInclude
+#line 21 "/home/aluno/OpenFOAM/aluno-11/run/flange_mod/system/blockMeshDict/#codeStream"
+#include "pointField.H"
+//}}} end codeInclude
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+// * * * * * * * * * * * * * * * Local Functions * * * * * * * * * * * * * * //
+
+//{{{ begin localCode
+
+//}}} end localCode
+
+
+// * * * * * * * * * * * * * * * Global Functions  * * * * * * * * * * * * * //
+
+extern "C"
+{
+    void codeStream_49193cb8b23425a89b3f775bf00963f4f973b89b
+    (
+        Ostream& os,
+        const dictionary& dict
+    )
+    {
+//{{{ begin code
+        #line 26 "/home/aluno/OpenFOAM/aluno-11/run/flange_mod/system/blockMeshDict/#codeStream"
+pointField points(13);
+
+        points[0]   = point(0,         0,    0.5);
+        points[1]   = point(5,         0,    0.5);
+        points[2]   = point(15,        0,    0.5);
+        points[3]   = point(20,        0,    0.5);
+        points[4]   = point(6.46,   3.53,    0.5);
+        points[5]   = point(13.53,  3.53,    0.5);
+        points[6]   = point(0,        10,    0.5);
+        points[7]   = point(20,       10,    0.5);
+        points[8]   = point(0,       100,    0.5);
+        points[9]   = point(20,      100,    0.5);
+        points[10]  = point(100,     100,    0.5);
+        points[11]  = point(100,      10,    0.5);
+        points[12]  = point(100,       0,    0.5);
+
+        // Duplicate x points
+        label sz = points.size();
+        points.setSize(2*sz);
+        for (label i = 0; i < sz; i++)
+        {
+            const point& pt = points[i];
+            points[i+1*sz] = point( pt.x(), pt.y(), -pt.z());
+        }
+
+        label sz2 = points.size();
+        points.setSize(2*sz2);
+        for (label i = 0; i < sz2; i++)
+        {
+            const point& pt = points[i];
+            points[i+1*sz2] = point(-pt.x(), pt.y(),  pt.z());
+        }
+
+        os  << points;
+//}}} end code
+    }
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+// ************************************************************************* //
+
